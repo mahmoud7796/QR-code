19905a11d25876b38d63122af0ecced3a23430c5
Mahmoud Diab <mahmoud.diab@initiumsolutions.me>
2022-01-30 11:04:28 +0200

    QR-code

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class HomeController extends BaseController
+{
+
+
+
+
+}
